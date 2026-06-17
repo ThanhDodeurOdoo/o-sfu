c99ffd655288bf82c6302546df42d1bfd56fe6b6
ThanhDodeurOdoo <tso@odoo.com>
2026-06-17 12:28:53 +0200

    [FIX] core: restore packet hot path branches

--- crates/core/src/engine/media_transport/rtc/commands.rs
+++ crates/core/src/engine/media_transport/rtc/commands.rs
@@ -135,7 +135,11 @@ impl RemoteSourceControl {
     fn send_command(&self, command: RtcWorkerCommand, drop_kind: RtcRemoteControlDropKind) -> bool {
         match self.tx.try_send(command) {
             Ok(()) => true,
-            Err(_error) => {
+            Err(mpsc::error::TrySendError::Full(_command)) => {
+                self.metrics.record_rtc_remote_control_drop(drop_kind);
+                false
+            }
+            Err(mpsc::error::TrySendError::Closed(_command)) => {
                 self.metrics.record_rtc_remote_control_drop(drop_kind);
                 false
             }
